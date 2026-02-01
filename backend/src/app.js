8e59ace86967c21f49a377abf51318c5988b82ac
Yuvrajsingh1256 <yuvrajsingh6437@gmail.com>
2026-02-02 02:05:44 +0530

    Initial commit: AI code review app

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import aiRoutes from "./routes/ai.routes.js";
+import cors from "cors";
+
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/ai", aiRoutes);
+
+export default app;
